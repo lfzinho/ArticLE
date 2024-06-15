3b09c7d410c4f7eed4e4fbe7a191a02b80c93933
BrunoFornaro <brunopereirafornaro@gmail.com>
2024-06-14 23:50:29 -0300

    docs: methods docstring

--- sheets_connection.py
+++ sheets_connection.py
@@ -1,3 +1,7 @@
+'''
+Classe para salvar os dados no Google Sheets
+'''
+
 import os.path
 import pandas as pd
 from googleapiclient.discovery import build
@@ -9,6 +13,15 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '1R4Wo4gQG-zopWZvrpGz5WJtKkkSr9ULrwTUmRg9X29Y'
 
 class SaveMetrics():
+    '''
+    Classe para salvar os dados no Google Sheets
+
+    Parâmetros
+    ----------
+    range : str
+        Range do sheet onde os dados serão salvos. O padrão é 'A:AZ'. Utiliza 
+        a sintaxe do Google Sheets para definir o range. Exemplo: 'A:AZ' ou 'A1:AZ100'
+    '''
     def __init__(self, range:str='A:AZ') -> None:
         self.range = range
         self.sheet = build(
@@ -18,6 +31,14 @@ class SaveMetrics():
         ).spreadsheets()
 
     def _set_creds(self) -> Credentials:
+        '''
+        Função para setar as credenciais do Google Sheets
+        
+        Retorna
+        -------
+        Credentials
+            Credenciais do Google Sheets
+        '''
         creds = None
         if os.path.exists('token.json'):
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -31,6 +52,20 @@ class SaveMetrics():
         return creds
 
     def append_metrics(self, type_of_metric:str, data:pd.DataFrame) -> None:
+        '''
+        Função para salvar os dados no Google Sheets
+
+        Parâmetros
+        ----------
+        type_of_metric : str
+            Tipo de métrica que será salva no Google Sheets
+        data : pd.DataFrame
+            DataFrame com os dados que serão salvos no Google Sheets
+
+        Retorna
+        ----------
+        None
+        '''
         # Define a página e o range
         range = f"{type_of_metric}!{self.range}"
 
