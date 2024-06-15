7d8f6bd0e1dd7741c93ecaa9c8cdc44dc2db40fa
BrunoFornaroRdP <bruno.fornaro@reidopitaco.com>
2024-06-14 23:00:56 -0300

    refactor: clean code

--- sheets_connection.py
+++ sheets_connection.py
@@ -6,18 +6,13 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SAMPLE_SPREADSHEET_ID = '1R4Wo4gQG-zopWZvrpGz5WJtKkkSr9ULrwTUmRg9X29Y'
-SPECIFIC_QUERY_RANGE = 'specific_query!A:B'
-GENERIC_QUERY_RANGE = 'generic_query!A:E'
+SPREADSHEET_ID = '1R4Wo4gQG-zopWZvrpGz5WJtKkkSr9ULrwTUmRg9X29Y'
 
 class SaveMetrics():
-    def __init__(self) -> None:
+    def __init__(self, range='A:AZ') -> None:
         self.creds = self._set_creds()
         self.service = build('sheets', 'v4', credentials=self.creds)
-        self.ranges = {
-            'specific_query': 'specific_query!A:B',
-            'generic_query': 'generic_query!A:AZ',
-        }
+        self.range = range
 
     def _set_creds(self):
         creds = None
@@ -33,18 +28,20 @@ class SaveMetrics():
         return creds
 
     def append_metrics(self, type_of_metric, data):
+        range = f"{type_of_metric}!{self.range}"
         sheet = self.service.spreadsheets()
         result = sheet.values().get(
-            spreadsheetId=SAMPLE_SPREADSHEET_ID,
-            range=self.ranges[type_of_metric]
+            spreadsheetId=SPREADSHEET_ID,
+            range=range
         ).execute()
         values = result.get('values', [])
 
         for row in data:
             values.append(row)
         result = sheet.values().update(
-            spreadsheetId=SAMPLE_SPREADSHEET_ID,
-            range=self.ranges[type_of_metric], 
+            spreadsheetId=SPREADSHEET_ID,
+            range=range, 
             valueInputOption="RAW",
             body={"values": values}
         ).execute()
+
